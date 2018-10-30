6cb6603ec2a5fb4540ccc9da9360fde8da371439
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-10-30 10:50:00 -0300

    csv is loaded

--- coordinators/replicator.py
+++ coordinators/replicator.py
@@ -1,37 +1,54 @@
+import csv
 import glob
-import pandas as pd
 
-from middleware.connection import ReplicationSocket
+#from middleware.connection import ReplicationSocket
 
 class DataReplicator(object):
 
     def __init__(self, data, pattern, port):
         self.data_path = data
-        self.patterns = pattern.strip('[]').replace(' ', '').split(',')
-        self.socket = ReplicationSocket(port)
+        self.patterns = pattern.strip('[]').replace(' ', '_').split(',')
+        #self.socket = ReplicationSocket(port)
+        self.NEW_DATA = 1
 
     def _parse_data(self):
 
-        files = []
+        rows = []
 
+        # Load csv files
         for file_path in glob.glob(self.data_path):
-            files.append(pd.read_csv(file_path))
+            with open(file_path, newline='') as csvf:
+                reader = csv.DictReader(csvf)
+                for row in reader:
+                    rows.append(row)
 
-        data = pd.concat(files, ignore_index=True)
+        return rows
 
-        data.columns = data.columns.str.replace(' ', '_')
+    def _parse_row(self, row):
 
-        for pattern in self.patterns:
-            new, old = pattern.split('=')
-            data.columns = data.columns.str.replace(old, new)
+        new_row = {}
 
-        return data
+        # Remove all spaces and convert it to '_'
+        for key in row.keys():
+            new_key = key.replace(' ', '_')
+            new_row[new_key] = row[key]
+
+        # Transform row (dictionary) in a string
+        # to send, considering the pattern mapping
+        parsed_row = ""
+        for mapping in self.patterns:
+            new, old = mapping.split('=')
+            parsed_row += new + '=' + new_row[old] + '\n'
+
+        return parsed_row
 
     def _send_data(self, data):
 
         for row in data:
+            row = self._parse_row(row)
             msg = "{data_id} {data_row}".format(data_id=self.NEW_DATA, data_row=row)
-            self.socket.send_string(msg)
+            #self.socket.send(msg)
+            print(msg)
 
     def run(self):
 
--- nodes/main.py
+++ nodes/main.py
@@ -6,9 +6,9 @@ sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 
 from coordinators.replicator import DataReplicator
 
-def main(data, pattern):
+def main(data, pattern, port):
 
-    replicator = DataReplicator(data, pattern)
+    replicator = DataReplicator(data, pattern, port)
 
     replicator.run()
 
@@ -26,6 +26,11 @@ if __name__ == '__main__':
             '--pattern',
             help='Match pattern for data fields'
     )
+    parser.add_argument(
+            '--port',
+            type=int,
+            default=5555
+    )
     args = parser.parse_args()
 
-    main(args.data, args.pattern)
+    main(args.data, args.pattern, args.port)
