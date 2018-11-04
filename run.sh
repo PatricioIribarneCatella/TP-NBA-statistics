f2b0e05a50d3951df55ecf37bcbe28cf53da26b9
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2018-11-03 21:17:40 -0300

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,12 +1,23 @@
+#!/bin/bash
+
+# Virtualenv setup and PyZmq installation
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
+# Run all the Nodes
 python3 nodes/filter.py --pattern="shot_result=SCORED" &
 python3 nodes/match_summary_worker.py --reducers=3 &
 python3 nodes/worker_reducer_proxy.py --wport=8888 --rport=9999 &
-python3 nodes/match_summary_reducer.py --rport=9999 --jport=10000 --rid=1 &
-python3 nodes/match_summary_reducer.py --rport=9999 --jport=10000 --rid=2 &
-python3 nodes/match_summary_reducer.py --rport=9999 --jport=10000 --rid=3 &
+
+for id in {1..3}
+do
+	python3 nodes/match_summary_reducer.py --rport=9999 --jport=10000 --rid=$id &
+done
+
+#python3 nodes/match_summary_reducer.py --rport=9999 --jport=10000 --rid=1 &
+#python3 nodes/match_summary_reducer.py --rport=9999 --jport=10000 --rid=2 &
+#python3 nodes/match_summary_reducer.py --rport=9999 --jport=10000 --rid=3 &
+
 python3 nodes/match_summary.py --jport=10000 --reducers=3 &
 
